12d4f91b229230143ca5dfbb5aae90e175ec1f0c
Professor Wang Matrix PhD <professor.wang.matrix.phd@gmail.com>
2018-12-02 22:43:39 +0000

    add --structure-only flag

--- src/dump_sniffer_clojure/core.clj
+++ src/dump_sniffer_clojure/core.clj
@@ -3,17 +3,28 @@
   (require [clojure.tools.cli :as cli])
   (:gen-class))
 
-(defn dump-sniffer-table-names [fname]
+(defn extract-table-names [fname]
   (->> (slurp fname)
        (re-seq #"(?i)create table `(.*?)`")
        (vec)
        (map last)))
 
+(defn extract-db-structure [fname]
+  (->> (slurp fname)
+       (re-seq #"(?is)DROP TABLE .*?;|CREATE TABLE.*?;")
+       (vec)
+       (clojure.string/join "\n")))
+
 (defn -main
   [& args]
-  (let [options (cli/parse-opts args [["-t" "--table-names"]])]
-    (if (get-in options [:options :table-names])
-      (->> (dump-sniffer-table-names (last args))
-           (clojure.string/join "\n")
-           println)
-      (println "doing nothing"))))
+  (let [options (cli/parse-opts args [["-t" "--table-names"] ["-s" "--structure-only"]])]
+    (cond
+      (get-in options [:options :table-names])
+        (->> (extract-table-names (last args))
+             (clojure.string/join "\n")
+             println)
+      (get-in options [:options :structure-only])
+        (->> (extract-db-structure (last args))
+             (print))
+      :else
+        (println "doing nothing"))))
--- test/dump_sniffer_clojure/core_test.clj
+++ test/dump_sniffer_clojure/core_test.clj
@@ -2,6 +2,15 @@
   (:require [clojure.test :refer :all]
             [dump-sniffer-clojure.core :refer :all]))
 
+(def table-structure-output
+  (clojure.string/join
+    "\n"
+    ["DROP TABLE IF EXISTS `customers`;"
+     "CREATE TABLE `customers` ("
+     "  `customerNumber` int(11) NOT NULL,"
+     "  `customerName` varchar(50) NOT NULL,"
+     ") ENGINE=InnoDB DEFAULT CHARSET=latin1;"]))
+
 (deftest test-main
   (def table-names-output
     (clojure.string/join "\n" ["customers" "employees" "offices" "orderdetails" "orders" "payments" "productlines" "products" ""]))
@@ -12,9 +21,22 @@
 
   (testing "with --table-names flag"
     (is (= (with-out-str (-main "--table-names" "test/fixtures/simple.sql"))
-           table-names-output))))
+           table-names-output)))
 
-(deftest test-dump-sniffer-table-names
+  (testing "with -s flag"
+    (is (= (with-out-str (-main "-s" "test/fixtures/single_table_with_data.sql"))
+           table-structure-output)))
+
+  (testing "with --structure-only flag"
+    (is (= (with-out-str (-main "-s" "test/fixtures/single_table_with_data.sql"))
+           table-structure-output))))
+
+(deftest test-extract-table-names
   (testing "outputs table names"
-    (is (= (dump-sniffer-table-names "test/fixtures/simple.sql")
+    (is (= (extract-table-names "test/fixtures/simple.sql")
            ["customers" "employees" "offices" "orderdetails" "orders" "payments" "productlines" "products"]))))
+
+(deftest test-extract-db-structure
+  (testing "outputs db structure"
+    (is (= (extract-db-structure "test/fixtures/single_table_with_data.sql")
+           table-structure-output))))
